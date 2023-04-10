eb6a8f9acb2391edd26239926a25a33c6df37eb3
hossmi <hossmi@gmail.com>
2023-04-10 21:31:33 +0200

    Add Index localizations

---
+++ SampleWeb/Pages/IIndexLocalizations.cs
@@ -0,0 +1,13 @@
+﻿using InjectedLocalizations;
+
+namespace SampleWeb.Services.Localizations
+{
+    public interface IIndexLocalizations : ILocalizations
+    {
+        string Home_page { get; }
+        string Welcome { get; }
+        string Learn_about { get; }
+        string building_Web_apps_with_Asp_Net_Core { get; }
+        string Current_culture { get; }
+    }
+}
