66b3b335bf7d3f4928fe3147e83b7b2fa7226f58
hossmi <hossmi@gmail.com>
2023-04-23 14:15:16 +0200

    Add concrete spanish IIndexLocalizations implementation

---
+++ SampleWeb/Pages/CastellanoIndexLocalizations.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace SampleWeb.Services.Localizations
+{
+    public class CastellanoIndexLocalizations : IIndexLocalizations
+    {
+        public string Home_page { get; } = "Página inicial";
+        public string Welcome { get; } = "Bienvenido";
+        public string Learn_about { get; } = "Aprenda sobre";
+        public string building_Web_apps_with_Asp_Net_Core { get; } = "crear aplicaciones web mediante Asp.Net Core";
+        public string Current_culture { get; } = "Idioma actual";
+        public CultureInfo Culture { get; } = new CultureInfo("es-ES");
+    }
+}
